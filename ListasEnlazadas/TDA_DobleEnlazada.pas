b8246a18f73a321a9c92de96c7f9faf53e65f516
Pablo Veláquez <3650748960101@ingenieria.usac.edu.gt>
2025-08-03 23:22:00 -0600

    Ingresamos un ejemplo de lista enlazada simple

--- ListasEnlazadas/TDA_DobleEnlazada.pas
+++ ListasEnlazadas/TDA_DobleEnlazada.pas
@@ -1,4 +1,4 @@
-program TDA_Prioridad2;
+program TDA_DobleEnlazada;
 
 {$mode objfpc}{$H+}
 
@@ -53,12 +53,12 @@ type
         procedure MostrarCola;
     end;
 
-
+//Procdimiento para insertar 
 procedure TColaPrioridad.InsertarOrdenado(APersona: TPersona);  //Procedimiento para hacer las Incersiones 
 var
     Nuevo, Actual: TNodo;
 begin
-    Nuevo := TNodo.Create(APersona);    
+    Nuevo := TNodo.Create(APersona); //Aquí creamos el nuevo nodo, recibiendo una persona (Nombre y prioridad) 
 
     //Si la cola está vacía
     if Cabeza = nil then 
@@ -71,6 +71,8 @@ begin
     Actual := Cabeza;
 
     //Buscamos la posición adecuada según la prioridad que tenga
+    //Si Actual no es el nodo final Y la prioridad del nuevo Nodo es mayor a la Prioridad del Nodo Actual
+    //Seguira recorriendo la lista y ver donde ira el nuevo Nodo.
     while(Actual <> nil) and (APersona.Prioridad >= Actual.Persona.Prioridad) do 
         Actual := Actual.Siguiente;
 
@@ -93,10 +95,10 @@ begin
     //Insertar en medio
     else
     begin
-        Nuevo.Siguiente := Actual;
-        Nuevo.Anterior := Actual.Anterior;
-        Actual.Anterior.Siguiente := Nuevo;
-        Actual.Anterior := Nuevo;
+        Nuevo.Siguiente := Actual;  //El apuntador siguiente del nuevo Nodo apuntara al Nodo Actual
+        Nuevo.Anterior := Actual.Anterior;  //El apuntador Anterior del nuevo Nodo apuntara al anterior que apuntaba el Actual
+        Actual.Anterior.Siguiente := Nuevo; //El apuntador siguiente del nodo anterior de Actual, ahora apuntara al Nuevo nodo
+        Actual.Anterior := Nuevo; //El apuntador Anterior de Actual apuntara al Nodo nuevo.
     end;
 end;
 
@@ -118,7 +120,7 @@ var
   Cola: TColaPrioridad;
   i: Integer;
 begin
-  // Crear las personas (orden de entrada)
+  //Crear las personas (orden de entrada)
   Personas[0] := TPersona.Create('Pedro', 3);
   Personas[1] := TPersona.Create('Don Luis', 1);
   Personas[2] := TPersona.Create('Maria', 2);
@@ -128,18 +130,18 @@ begin
   Personas[6] := TPersona.Create('Alejandra', 2);
   Personas[7] := TPersona.Create('Pepe', 3);
 
-  // Mostrar orden de entrada
+  //Mostrar orden de entrada
   WriteLn('Orden de entrada (cola sin prioridad):');
   for i := 0 to High(Personas) do
     WriteLn(Personas[i].ToString);
 
-  // Crear cola y agregar ordenadamente
+  //Crear cola y agregar ordenadamente
   Cola := TColaPrioridad.Create;
   for i := 0 to High(Personas) do
     Cola.InsertarOrdenado(Personas[i]);
 
-  // Mostrar ordenado por prioridad
+  //Mostrar ordenado por prioridad
   WriteLn;
   WriteLn('Cola ordenada por prioridad:');
-  Cola.MostrarCola;
+  Cola.MostrarCola;  //Llamamos al procedimiento para que ejecute la impresión de la cola
 end.
